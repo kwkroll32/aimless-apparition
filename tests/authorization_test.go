515c31bdabfbe63c248a29fbd234adbc19cf425b
Karl Kroll <kwkroll.32@gmail.com>
2016-04-09 10:12:37 -0400

    successfully launches new api instance

--- tests/authorization_test.go
+++ tests/authorization_test.go
@@ -10,7 +10,7 @@ import (
 
 // external packages
 import (
-   // "github.com/ChimeraCoder/anaconda"
+   "github.com/ChimeraCoder/anaconda"
    "github.com/kwkroll32/aimless-apparition/Settings"
 )
 
@@ -24,9 +24,41 @@ func TestParseTwitterSettingsJson(t *testing.T) {
     if err = jsonParser.Decode(&twitterSettings); err != nil {
         t.Errorf("cannot parse the twitter settings json file: ../secret_keys.json")
     }
-    
+    if len(twitterSettings.AccessToken) == 0 {
+        t.Errorf("twitter access token is empty!")
+    }
+    if len(twitterSettings.AccessTokenSecret) == 0 {
+        t.Errorf("twitter access token secret is empty!")
+    }
+    if len(twitterSettings.ConsumerKey) == 0 {
+        t.Errorf("twitter consumer key is empty!")
+    }
+    if len(twitterSettings.ConsumerSecret) == 0 {
+        t.Errorf("twitter consumer secret is empty!")
+    }
 }
- 
+
+func TestLaunchNewAPI(t *testing.T) {
+    twitterSettingsFile, err := os.Open("../secret_keys.json")
+    if err != nil {
+        t.Errorf("cannot open the twitter settings json file: ../secret_keys.json")
+    }
+    twitterSettings := TwitterSettings.TwitterSettings{}
+    jsonParser := json.NewDecoder(twitterSettingsFile)
+    if err = jsonParser.Decode(&twitterSettings); err != nil {
+        t.Errorf("cannot parse the twitter settings json file: ../secret_keys.json")
+    }
+    
+    anaconda.SetConsumerKey(twitterSettings.ConsumerKey)
+    anaconda.SetConsumerSecret(twitterSettings.ConsumerSecret)
+    api := anaconda.NewTwitterApi(twitterSettings.AccessToken, twitterSettings.AccessTokenSecret)
+    ok, err := api.VerifyCredentials()
+    if err != nil {
+        t.Errorf("could not verify twitter credentials")
+    } else if !ok {
+        t.Errorf("twitter credentials invalid")
+    }
+}
 func TestMain(m *testing.M) {
     os.Exit(m.Run())
 }
